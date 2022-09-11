a650f1d2e52e5aefaa53ef18bad76e990b0c52ab
adesaputraa <adesaputra0928@gmail.com>
2022-09-11 21:35:05 +0700

    materi

--- cypress/e2e/0-personal-learn/login.cy.js
+++ cypress/e2e/0-personal-learn/login.cy.js
@@ -20,26 +20,27 @@ describe('Working with inputs', () => {
 
 
     // INI TUGAS mengaktifkan fitur checkbox keep me signed in
-    it('Should fill Checkbox', () => {
-        cy.get('input[name="user_remember_me"]').click();
-        cy.get('[type="checkbox"]').type('checkbox')
-    });
+    // it('Should fill Checkbox', () => {
+    //     cy.get('input[name="user_remember_me"]').click();
+    //     cy.get('[type="checkbox"]').type('checkbox')
+    // });
 
     it('Should try to login', () => {
         cy.fixture("user").then(user => {
             const username = user.username
             const password = user.password
 
-            cy.get('#user_login').clear()
-            cy.get('#user_login').type(username)
+            // Memanggil Commands dari folder support
+            cy.login(username, password)
 
-            cy.get('input[name="user_password"]').clear()
-            cy.get('input[name="user_password"]').type(password)
-
-            cy.get('[type="submit"]').click()
+            // cy.get('#user_login').clear()
+            // cy.get('#user_login').type(username)
+            // cy.get('input[name="user_password"]').clear()
+            // cy.get('input[name="user_password"]').type(password)
+            // cy.get('[type="submit"]').click()
 
             //clas menggunakan titik
-            cy.get('.alert-error').should('contains.text', 'Login and/or password are wrong.')
+            // cy.get('.alert-error').should('contains.text', 'Login and/or password are wrong.')
 
         })
     });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+
+    cy.get('#user_login').clear()
+    cy.get('#user_login').type(username)
+
+    cy.get('input[name="user_password"]').clear()
+    cy.get('input[name="user_password"]').type(password)
+
+    cy.get('[type="submit"]').click()
+})
+
+Cypress.Commands.add('paybills', () => {
+
+    cy.get('#sp_payee').select('apple').should('have.value', 'apple')
+    cy.get('#sp_account').select('4').should('have.value', '4')
+    cy.get('#sp_amount').type("20")
+    cy.get('#sp_date').click();
+    cy.contains('28').click();
+    cy.get('#sp_description').type("ade")
+
+
+
+})
+
+
+
+
